89c4da4d5f39fe6df8841a28c734facc889494b3
bhanuka.15@cse.mrt.ac.lk <951483060@Rc>
2020-07-07 15:39:28 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WineComponent } from './wine/wine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WineComponent
   ],
   imports: [
     BrowserModule,
